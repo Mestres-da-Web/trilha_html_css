5a04011e62e8a858231d0978a95beffc9b41c3ac
Jancer Lima <contato.jancersl@gmail.com>
2022-10-01 20:19:18 -0300

    V4

---
+++ HTML/Projeto exercício/V4/components/Header/index.js
@@ -0,0 +1,22 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const baseURL = "/HTML/Projeto exercício/V3";
+    this.innerHTML = `
+      <header>
+        <nav>
+          <ul>
+            <li><a href="${baseURL}/">Início</a></li>
+            <li><a href="${baseURL}/pages/Contact/">Contato</a></li>
+            <li><a href="${baseURL}/pages/Gallery">Galeria</a></li>
+            <li><a href="${baseURL}/pages/Products">Produtos</a></li>
+          </ul>
+        </nav>
+      </header>
+    `;
+  }
+}
+customElements.define("header-component", Header);
